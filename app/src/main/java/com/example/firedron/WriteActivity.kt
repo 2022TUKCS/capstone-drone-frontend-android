ca1b89d589e6c699159b7b8613c523f3a039c808
KimJungYeon <jungyeon9130@kpu.ac.kr>
2022-03-08 04:55:25 +0900

    레이아웃 수정

--- app/src/main/java/com/example/firedron/WriteActivity.kt
+++ app/src/main/java/com/example/firedron/WriteActivity.kt
@@ -2,10 +2,15 @@ package com.example.firedron
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.firedron.databinding.ActivityWriteBinding
 
 class WriteActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityWriteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_write)
+
+        binding = ActivityWriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
