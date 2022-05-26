73e46a7b2152090fb21f922e1f05591bed3bfe9a
Sean Hong <seanhong2000@gmail.com>
2022-05-27 02:01:20 +0900

    Add Device FCM Token to be sent to main backend server + some minor changes when signing up.

--- app/src/main/java/com/example/firedron/Service/SignupService.kt
+++ app/src/main/java/com/example/firedron/Service/SignupService.kt
@@ -1,17 +1,22 @@
 package com.example.firedron.Service
 import com.example.firedron.dto.Auth
+import com.example.firedron.dto.UToken
 import retrofit2.Call
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface SignupService {
-
     @FormUrlEncoded
     @POST("auth/users/")
     fun requestSignup(   //input 정의
         @Field("username") username:String,
         @Field("password") password:String,
-        @Field("email") email:String
+        @Field("email") email:String,
     ) : Call<Auth> //output 정의
+
+    @FormUrlEncoded
+    @PUT("api/v1/user/{uid}/")
+    fun putUserFCMToken(
+        @Path(value = "uid", encoded = true) uid: String,
+        @Field("token") token: String,
+    ) : Call<UToken>
 }
